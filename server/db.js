3b0e84b7f149cdfbe1468939082943c07aa6992e
Ashwik <ashwikpampati@gmail.com>
2022-12-02 21:57:46 -0500

    db fixed

--- server/db.js
+++ server/db.js
@@ -1,7 +1,8 @@
 import mongoose from "mongoose";
 
-const Connection = async () => {
-  const URL = `mongodb+srv://root:root@newsapp.qqtktgg.mongodb.net/?retryWrites=true&w=majority`;
+const Connection = async (username, password) => {
+  // const URL = `mongodb+srv://root:root@newsapp.qqtktgg.mongodb.net/?retryWrites=true&w=majority`;
+  const URL = `mongodb+srv://${username}:${password}@newsapp.qqtktgg.mongodb.net/?retryWrites=true&w=majority`;
   try {
     await mongoose.connect(URL, { useNewUrlParser: true });
     console.log("Database connected successfully");
--- server/index.js
+++ server/index.js
@@ -16,7 +16,7 @@ const PORT = 8000;
 const username = process.env.DB_USERNAME;
 const password = process.env.DB_PASSWORD;
 
-Connection();
+Connection(username, password);
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
 
